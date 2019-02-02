dc179903d7e74a034d9c09f4b91e0e8d9bad98cc
bj-Charles <bjcharles96@gmail.com>
2019-02-02 18:34:33 -0500

    Implemented new weapons for main driver
    Added functionality to use the new weapon classes in weapon factory
    and Main.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -1,14 +1,17 @@
-/* 
+/*
  * File:   WeaponFactory.cpp
  * Author: Javier <jrescobara@gmail.com>
- *  
- */ 
+ *
+ */
 
 #include <string>
-#include <stddef.h> 
+#include <stddef.h>
 #include "WeaponFactory.h"
-#include "CommonSword.h" 
+#include "CommonSword.h"
 #include "CommonSpear.h"
+#include "SimpleHammer.h"
+#include "CrazyRandomSword.h"
+#include "OdinSpear.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -23,9 +26,24 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
+
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("hammer") == 0)
+    {
+        return new SimpleHammer();
+    }
+
+    if (name.compare("crazy sword") == 0)
+    {
+      return new CrazyRandomSword();
+    }
+
+    if (name.compare("odin spear") == 0)
+    {
+      return new OdinSpaer();
     }
 
     throw "Invalid weapon";
